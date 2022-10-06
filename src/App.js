6833a393b0c7db4668e331df5c00ad0e0c9681f3
MonikaRaghav <105781189+MonikaRaghav@users.noreply.github.com>
2022-10-06 12:40:59 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div >
+      <header >
+        <a href="/">Ecommerce Website</a>
+        
+      </header>
+      <main>
+          List
+        </main>
+    </div>
+  );
+}
+
+export default App;
